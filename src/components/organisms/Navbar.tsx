3430882ec4ed1661debf1a5100f92fe259bdf856
Augustin <rjja@outlook.fr>
2023-09-02 19:00:40 +0300

    update navlinks

---
+++ src/components/organisms/Navbar.tsx
@@ -0,0 +1,10 @@
+import CustomNavigation from "../molecules/CustomNavigation";
+import React from "react";
+import {navlinks} from "../../constants/constants";
+
+export default function Navbar() {
+    return <CustomNavigation
+        title={"School"}
+        navliks={navlinks}
+    />;
+}
--- src/types/students.ts
+++ src/types/students.ts
@@ -1,7 +1,12 @@
 
-export interface Students {
+export interface StudentsType {
     id: number;
     name: string;
     age: number;
     email: string;
+}
+
+export interface navlinksType {
+    name: string;
+    path: string;
 }
