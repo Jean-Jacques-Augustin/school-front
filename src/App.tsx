d2e788d816803a878011503f732039ba3c62a7e7
Elony <manampy.abraham@yahoo.fr>
2023-09-02 16:21:47 +0200

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import {Box} from "@mui/material";
-import {Route, Routes} from "react-router-dom";
+import "./App.css";
+import { Box } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
 
 /**
  * @name App
@@ -11,19 +11,19 @@ import {Route, Routes} from "react-router-dom";
  */
 
 function App() {
-    return (
-        <div className="App">
-            <Box>
-                <div>Navigation</div>
-                <Routes>
-                    <Route path="/" element={<div>Home</div>}/>
-                    <Route path="/login" element={<div>Login</div>}/>
-                    <Route path="/signup" element={<div>Signup</div>}/>
-                    <Route path="*" element={<div>404</div>}/>
-                </Routes>
-            </Box>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Box>
+        <div>Navigation</div>
+        <Routes>
+          <Route path="/" element={<div>Home</div>} />
+          <Route path="/login" element={<div>Login</div>} />
+          <Route path="/signup" element={<div>Signup</div>} />
+          <Route path="*" element={<div>404</div>} />
+        </Routes>
+      </Box>
+    </div>
+  );
 }
 
 export default App;
