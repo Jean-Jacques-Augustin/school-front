ff36619f27732a5929f49f3459b92b44b42ece15
Augustin <rjja@outlook.fr>
2023-09-03 19:03:26 +0300

    update

--- src/components/atoms/CustomTextField.tsx
+++ src/components/atoms/CustomTextField.tsx
@@ -1,15 +1,45 @@
 import React from "react";
 import { styled, TextField } from "@mui/material";
 
-interface CustomLoginInterface {
-  children?: React.ReactNode;
+// interface CustomLoginInterface {
+//   children?: React.ReactNode;
+// }
+
+// const StyledCustomLogin = styled(TextField)`
+//   color: inherit;
+//   text-decoration: none;
+// `;
+
+// export default function CustomLogin({ children }: CustomLoginInterface) {
+//   return <StyledCustomLogin>{children}</StyledCustomLogin>;
+// }
+
+interface CustomTextFieldInterface {
+    label: string;
+    onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+    value?: string;
 }
 
-const StyledCustomLogin = styled(TextField)`
-  color: inherit;
-  text-decoration: none;
+const StyledCustomTextField = styled(TextField)`
+     {
+        color: inherit;
+        text-decoration: none;
+        width: 100%;
+    }
 `;
 
-export default function CustomLogin({ children }: CustomLoginInterface) {
-  return <StyledCustomLogin>{children}</StyledCustomLogin>;
+export default function CustomTextField({
+    label,
+    onChange,
+    value,
+}: CustomTextFieldInterface) {
+    return (
+        <StyledCustomTextField
+            fullWidth
+            label={label}
+            variant="outlined"
+            onChange={onChange}
+            value={value}
+        />
+    );
 }
--- src/components/organisms/NavLogin.tsx
+++
@@ -1,9 +0,0 @@
-import CustomLoginStudents from "../molecules/CustomLoginStudents";
-import React from "react";
-import { loginAllStudents } from "../../constants/constants";
-
-export default function NavLogin() {
-  return (
-    <CustomLoginStudents loginAllStudents={loginAllStudents} password="" />
-  );
-}
